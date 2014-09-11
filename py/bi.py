2afc082e7518c0f492258e38f968122c7aa132b9
brianSan <briansan24@gmail.com>
2014-09-11 16:15:49 -0400

    rewrote the top level script as a class

--- py/bi.py
+++ py/bi.py
@@ -4,82 +4,94 @@
 # description: sets up the whole interface with nonfunctional inputs
 #
 
+import pygtk
+pygtk.require( '2.0' )
 import gtk
 
-# app
-w = gtk.Window()
+class breadinterface():
+  # app
+  w = gtk.Window()
 
-# controller
-frame = gtk.VBox()
+  # controller
+  frame = gtk.VBox()
 
-# frame
-top = gtk.HBox()
-bottom = gtk.HBox()
-tree = gtk.TreeView()
+  # frame
+  top = gtk.HBox()
+  bottom = gtk.HBox()
+  tree = gtk.TreeView()
 
-# button labels
-tl_label = unichr(9776)     # menu
-tm_label = "breadinterface" # title
-tr_label = "i"				# info
-bl_label = "_"				# remove
-bm_label = "these buttons don't do anything"
-br_label = "+"				# add
+  # button labels
+  tl_label = unichr(9776)     # menu
+  tm_label = "breadinterface" # title
+  tr_label = "i"				# info
+  bl_label = unichr(2212)		  # remove
+  bm_label = "these buttons don't do anything"
+  br_label = "+"				# add
 
-button_dict = {
-  tl_label : "clicking the menu button opens a subframe of menu items",
-  tm_label : "clicking the title button does not do anything",
-  tr_label : "clicking the info button will display more information about this (nothing)",
-  bl_label : "clicking the remove button will remove nothing",
-  bm_label : "clicking the bottom middle button will do absolutely nothing",
-  br_label : "clicking the add button will add nothing"
-}
+  """
+  button_dict = {
+    tl_label : "clicking the menu button opens a subframe of menu items",
+    tm_label : "clicking the title button does not do anything",
+    tr_label : "clicking the info button will display more information about this (nothing)",
+    bl_label : "clicking the remove button will remove nothing",
+    bm_label : "clicking the bottom middle button will do absolutely nothing",
+    br_label : "clicking the add button will add nothing"
+  }
+  """
 
-# bar
-tl = gtk.Button(tl_label)
-tm = gtk.Button(tm_label)
-tr = gtk.Button(tr_label)
-bl = gtk.Button(bl_label)
-bm = gtk.Button(bm_label)
-br = gtk.Button(br_label)
+  # bar
+  tl = gtk.Button(tl_label)
+  tm = gtk.Button(tm_label)
+  tr = gtk.Button(tr_label)
+  bl = gtk.Button(bl_label)
+  bm = gtk.Button(bm_label)
+  br = gtk.Button(br_label)
 
-def init():
-  w.set_size_request( 320, 480)
-  w.connect("delete-event", gtk.main_quit)
+  def init( self ):
+    self.w.set_size_request( 320, 480)
+    self.w.connect("delete-event", gtk.main_quit)
 
-  tl.set_size_request( 44, 44 )
-  tr.set_size_request( 44, 44 )
-  bl.set_size_request( 44, 44 )
-  br.set_size_request( 44, 44 )
+    self.tl.set_size_request( 44, 44 )
+    self.tr.set_size_request( 44, 44 )
+    self.bl.set_size_request( 44, 44 )
+    self.br.set_size_request( 44, 44 )
 
-  top.pack_start(tl,False)
-  top.pack_start(tm,True)
-  top.pack_start(tr,False)
+    self.tl.connect( "clicked", self.display_button_info )
+    self.tr.connect( "clicked", self.display_button_info )
+    self.bl.connect( "clicked", self.display_button_info )
+    self.br.connect( "clicked", self.display_button_info )
 
-  bottom.pack_start(bl,False)
-  bottom.pack_start(bm,True)
-  bottom.pack_start(br,False)
+    self.top.pack_start( self.tl,False)
+    self.top.pack_start( self.tm,True)
+    self.top.pack_start( self.tr,False)
 
-  frame.pack_start(top,False)
-  frame.pack_start(tree,True)
-  frame.pack_start(bottom,False)
+    self.bottom.pack_start( self.bl,False)
+    self.bottom.pack_start( self.bm,True)
+    self.bottom.pack_start( self.br,False)
 
-  w.add(frame)
+    self.frame.pack_start( self.top,False)
+    self.frame.pack_start( self.tree,True)
+    self.frame.pack_start( self.bottom,False)
 
-def display_button_info( button, info ):
-  message = gtk.MessageDialog(parent=w, 
+    self.w.add( self.frame)
+
+  def display_button_info( self, button ):
+    message = gtk.MessageDialog(parent=self.w, 
                             flags=0, 
                             type=gtk.MESSAGE_QUESTION, 
                             buttons=gtk.BUTTONS_OK, 
                             message_format=None)
-  message.set_markup( "" )
+    message.set_markup( button.get_label() ) # button, button_dict[ str(button.get_label()) ] )
+    message.run()
 
-def start():
-  w.show_all()
-  gtk.main()
+  def start( self ):
+    self.w.show_all()
+    gtk.main()
 
 def main():
-  init()
-  start()
+  bi = breadinterface()
+  bi.init()
+  bi.start()
 
 if __name__ == "__main__":
   main()
