0d5528c029459e490fcd3f6fd1563b6312c75563
bk <bkimstunnaboss@gmail.com>
2015-03-02 01:52:04 -0500

    - made the button labels to be white (not pretty) - fixed the button info stuff

--- py/BreadInterface/Controller.py
+++ py/BreadInterface/Controller.py
@@ -42,20 +42,6 @@ class Controller( ButtonLayout, Lifecycle ):
     self.bm = ButtonLayout.gtk_button(self.bm_label())
     self.br = ButtonLayout.gtk_button(self.br_label())
   
-    # fixing the size of the buttons
-    self.tl.set_size_request( 44, 44 )
-    self.tr.set_size_request( 44, 44 )
-    self.bl.set_size_request( 44, 44 )
-    self.br.set_size_request( 44, 44 )
-
-    # remove the borders
-    self.tl.props.relief = gtk.RELIEF_NONE
-    self.tm.props.relief = gtk.RELIEF_NONE
-    self.tr.props.relief = gtk.RELIEF_NONE
-    self.bl.props.relief = gtk.RELIEF_NONE
-    self.bm.props.relief = gtk.RELIEF_NONE
-    self.br.props.relief = gtk.RELIEF_NONE
-
     # hook up the button actions
     self.tl.connect( "clicked", self.tl_clicked )
     self.tm.connect( "clicked", self.tm_clicked )
@@ -63,14 +49,6 @@ class Controller( ButtonLayout, Lifecycle ):
     self.bl.connect( "clicked", self.bl_clicked )
     self.bm.connect( "clicked", self.bm_clicked )
     self.br.connect( "clicked", self.br_clicked )
-
-    # creating the button info dictionary
-    self.button_info_dict = {}
-    self.button_info_dict['tl'] = 'None'
-    self.button_info_dict['tr'] = 'None'
-    self.button_info_dict['bl'] = 'None'
-    self.button_info_dict['bm'] = 'None'
-    self.button_info_dict['br'] = 'None'
 
     # build the view
     self.top.pack_start( self.tl,False)
@@ -94,24 +72,23 @@ class Controller( ButtonLayout, Lifecycle ):
     return self.title
 
   def tm_clicked( self, v ):
-    msg = gtk.MessageDialog( parent=None,
-                             flags=0,
-                             type=gtk.MESSAGE_INFO,
-                             buttons=gtk.BUTTONS_NONE,
-                             message_format=None )
+    msg = gtk.MessageDialog( type=gtk.MESSAGE_INFO, 
+                             flags=gtk.DIALOG_MODAL,
+                             buttons=gtk.BUTTONS_OK)
+                             
     info = 'Button Layout\n\ntop-left: '+self.tl_label()+' : '
-    info += self.button_info_dict['tl']
+    info += self.tl_info()
     info += '\ntop-right: '+self.tr_label()+' : '
-    info += self.button_info_dict['tr']
+    info += self.tr_info()
     info += '\nbottom-left: '+self.bl_label()+' : '
-    info += self.button_info_dict['bl']
+    info += self.bl_info()
     info += '\nbottom-middle: '+self.bm_label()+' : '
-    info += self.button_info_dict['bm']
+    info += self.bm_info()
     info += '\nbottom-right: '+self.br_label()+' : '
-    info += self.button_info_dict['br']
-    info += '\n\nPress ESC twice to close this popup'
+    info += self.br_info()
     msg.set_markup( info )
     msg.run()
+    msg.destroy()
 
   def start( self ):
     self.frame.show_all()
@@ -124,6 +101,22 @@ class Controller( ButtonLayout, Lifecycle ):
     self.bl.set_label( self.bl_label() )
     self.bm.set_label( self.bm_label() )
     self.br.set_label( self.br_label() )
+
+    #
+    # set the color style on the label
+    # MUST BE DONE EVERY SINGLE TIME THE LABEL IS SET
+    # HOW STUPID IS THAT????
+    label = self.tl.get_child()
+    style = label.get_style().copy()
+    style.fg[gtk.STATE_NORMAL] = gtk.gdk.color_parse('#FFFFFF')
+    style.fg[gtk.STATE_PRELIGHT] = gtk.gdk.color_parse('#FFFFFF')
+    self.tl.get_child().set_style(style)
+    self.tm.get_child().set_style(style)
+    self.tr.get_child().set_style(style)
+    self.bl.get_child().set_style(style)
+    self.bm.get_child().set_style(style)
+    self.br.get_child().set_style(style)
+     
     self.view.update()
 
   def clear( self ):
